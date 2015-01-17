617171d0ac777caeb202642aed816cdaa6ec846f
Valerio Ponte <valerio.ponte@gmail.com>
2015-01-17 21:14:22 +0100

    switch to cdnizer, cdnize bootstrap and jquery too

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
 var gulp = require('gulp');
 var googlecdn = require('gulp-google-cdn');
+var cdnizer = require("gulp-cdnizer");
 
-gulp.task('default', function () {
-  return gulp.src('src/index.html')
-  .pipe(googlecdn(require('./bower.json'), {
-    cdn: {
-      angular: {
-        versions: ['1.3.9'],
-        url: function(version) {
-          return '//ajax.googleapis.com/ajax/libs/angularjs/' + version  +'/angular.min.js';
-        }
-      }
+gulp.task('cdnizer', function() {
+  gulp.src("./src/index.html")
+  .pipe(cdnizer([
+    'google:angular',
+    'cdnjs:jquery',
+    {
+      file: '**/bootstrap/**/*.css',
+      package: 'bootstrap',
+      cdn: '//maxcdn.bootstrapcdn.com/bootstrap/${ version }/css/${ filenameMin }' 
+    },
+    {
+      file: '**/bootstrap/**/*.js',
+      package: 'bootstrap',
+      cdn: '//maxcdn.bootstrapcdn.com/bootstrap/${ version }/js/${ filenameMin }' 
     }
-  }))
-  .pipe(gulp.dest('.'));
+  ]))
+  .pipe(gulp.dest("."));
 });
+
+gulp.task('watch', ['cdnizer'], function () {
+  gulp.watch('src/index.html', ['cdnizer']);
+});
+
+gulp.task('default', ['watch']);
